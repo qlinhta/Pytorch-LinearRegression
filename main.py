33a211b31ff0e2b89c029696c8a4336b1d945278
qlinhta <linhtq.sfu@gmail.com>
2022-02-27 16:03:13 +0100

    commit

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=1)
 
-X = torch.from_numpy(X_numpy.astype.float32)
-y = torch.from_numpy(y_numpy.astype.float32)
+X = torch.from_numpy(X_numpy.astype(np.float32))
+y = torch.from_numpy(y_numpy.astype(np.float32))
 y = y.view(y.shape[0], 1)
 n_samples, n_features = X.shape
 
@@ -40,6 +40,9 @@ for epoch in range(nums_epoch):
         print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
 
 #plot
-
+predicted = model(X).detach().numpy()
+plt.plot(X_numpy, y_numpy, 'ro')
+plt.plot(X_numpy, predicted, 'b')
+plt.show()
 
 
